6d414c73a0a051f97267e02e5854ec9e9a73f8f5
Dunaevsky Maxim <dunmaksim@users.noreply.github.com>
2024-11-10 23:02:39 +0300

    Add local changed files to main

---
+++ asciidoc-faces.el
@@ -0,0 +1,307 @@
+;;; asciidoc-faces.el --- Face definitions -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 Dunaevskii Maxim
+
+;; Author: Dunaevskii M. <dunmaksim@yandex.ru>
+;; Maintainer: Dunaevskii M. <dunmaksim@yandex.ru>
+;; Created: October 30, 2024
+;; Version: 0.0.1-alpha
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: AsciiDoc
+;; URL: https://github.com/dunmaksim/asciidoc-mode/
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file contains the face definitions for `asciidoc-mode'.
+
+;;; Code:
+
+(require 'faces)
+(require 'font-lock)
+
+(defgroup asciidoc-faces nil "Faces used in AsciiDoc Mode."
+  :group 'asciidoc
+  :group 'faces)
+
+
+
+(defface asciidoc-face-default '((t :inherit default))
+  "Face used for default text."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-default 'asciidoc-face-default)
+
+
+;; Attribute
+;; :name:value[]
+(defface asciidoc-face-attribute-name
+  '((t (:inherit font-lock-keyword-face)))
+  "Face for attribute name.")
+
+(defvar
+  asciidoc-face-attribute-name
+  'asciidoc-face-attribute-name
+  "Face name for attributes.")
+
+
+(defface asciidoc-face-attribute-value '((t (
+                                             :inhrit default
+                                             :foreground "blue")))
+  "Face for attribute name."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-attribute-value 'asciidoc-face-attribute-value)
+
+
+(defface asciidoc-face-header-1
+  '((t (:inherit font-lock-function-name-face)))
+  "Default face for headers at level 1."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-header-1 'asciidoc-face-header-1)
+
+
+
+(defface asciidoc-face-header-2
+  '((t (:inherit font-lock-function-name-face)))
+  "Default face for headers at level 2."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-header-2 'asciidoc-face-header-2)
+
+
+
+(defface asciidoc-face-header-3
+  '((t (:inherit font-lock-function-name-face)))
+  "Default face for headers at level 3."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-header-3 'asciidoc-face-header-3)
+
+
+
+(defface asciidoc-face-header-4
+  '((t (:inherit font-lock-function-name-face)))
+  "Default face for headers at level 4."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-header-4 'asciidoc-face-header-4)
+
+
+
+(defface asciidoc-face-header-5
+  '((t (:inherit font-lock-function-name-face)))
+  "Default face for headers at level 5."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-header-5 'asciidoc-face-header-5)
+
+
+
+(defface asciidoc-face-header-6
+  '((t (:inherit font-lock-function-name-face)))
+  "Default face for headers at level 6."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-header-6 'asciidoc-face-header-6)
+
+
+
+(defface asciidoc-face-comment
+  '((t (:inherit font-lock-comment-face)))
+  "Face used for comments."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-comment
+  'asciidoc-face-comment
+  "Face name for comment.")
+
+
+
+(defface asciidoc-face-emphasis '((t (
+                                      :inherit italic
+                                      :underline nil)))
+  "Face used for emphasis."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-emphasis 'asciidoc-face-emphasis)
+
+
+
+(defface asciidoc-face-bold '((t (:inherit bold)))
+  "Face used for bold."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-bold 'asciidoc-face-bold)
+
+
+(defface asciidoc-face-inline-code
+  '((t (:inherit font-lock-builtin-face)))
+  "Face for inline code."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-inline-code
+  'asciidoc-face-inline-code
+  "Face name for inline code.")
+
+
+
+(defface asciidoc-face-footnote '((t (:inherit warning)))
+  "Face for footnote: macros."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-footnote 'asciidoc-face-footnote)
+
+
+(defface asciidoc-face-footnote-text
+  '((t (:inherit font-lock-doc-face)))
+  "Face for footnote text."
+  :group 'asciidoc-faces)
+
+(defvar
+  asciidoc-face-footnote-text
+  'asciidoc-face-footnote-text
+  "Face name for footnote text.")
+
+
+
+(defface asciidoc-face-kbd '((t (:inherit font-lock-keyword-face)))
+  "Face for kbd: macros."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-kbd 'asciidoc-face-kbd "Face name for kbd macros name.")
+
+
+(defface asciidoc-face-kbd-text '((t (:inherit warning)))
+  "Face for :kbd:[Text] text."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-kbd-text 'asciidoc-face-kbd-text)
+
+
+;; ADMONITIONS
+
+(defface asciidoc-face-note '((t (:inherit (bold underline success))))
+  "Face for note adminition."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-note 'asciidoc-face-note)
+
+
+(defface asciidoc-face-tip '((t (:inherit (bold underline success))))
+  "Face for tip admonition."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-tip 'asciidoc-face-tip)
+
+
+(defface asciidoc-face-important '((t (:inherit (bold underline warning))))
+  "Face for important admonition."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-important 'asciidoc-face-important)
+
+
+(defface asciidoc-face-caution '((t (:inherit (bold underline error))))
+  "Face for warning admonition."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-caution 'asciidoc-face-caution)
+
+
+(defface asciidoc-face-warning '((t (:inherit (bold underline warning))))
+  "Face for warning admonition."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-warning 'asciidoc-face-warning)
+
+
+;; Block macro:
+;; name::value[Additional text]
+;;
+;; Inline macro:
+;; text name:value[Additional text] text
+
+(defface asciidoc-face-macro-name
+  '((t (:inherit (asciidoc-face-default font-lock-keyword-face))))
+  "Face for macro name."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-macro-name 'asciidoc-face-macro-name)
+
+(defface asciidoc-face-macro-value
+  '((t (:inherit (asciidoc-face-default font-lock-function-name-face))))
+  "Face for macro value."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-macro-value 'asciidoc-face-macro-value)
+
+(defface asciidoc-face-macro-text '((t (
+                                        :inherit asciidoc-face-default
+                                        :foreground "yellow")))
+  "Face for macro text."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-macro-text 'asciidoc-face-macro-text)
+
+
+(defface asciidoc-face-comment-delimiter
+  '((t (:inherit font-lock-comment-delimiter-face)))
+  "Face for comment delimiters."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-comment-delimiter
+  'asciidoc-face-comment-delimiter
+  "Face name to use for delimiters.")
+
+
+
+(defface asciidoc-face-bracket
+  '((t (:inherit font-lock-bracket-face)))
+  "Face used to highlight brackets, braces, and parens."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-bracket
+  'asciidoc-face-bracket
+  "Face name to use for brackets.")
+
+
+
+(defface asciidoc-face-punctuation
+  '((t (:inherit font-lock-punctuation-face)))
+  "Face for highlighting punctuation characters."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-punctuation
+  'asciidoc-face-punctuation
+  "Face name to use for punctuation.")
+
+
+
+(defface asciidoc-face-id
+  '((t (:inherit font-lock-variable-name-face)))
+  "Face for highlighting ID in text."
+  :group 'asciidoc-faces)
+
+(defvar asciidoc-face-id 'asciidoc-face-id)
+
+
+
+(provide 'asciidoc-faces)
+
+;;; asciidoc-faces.el ends here
---
+++ asciidoc-mode.el
@@ -0,0 +1,268 @@
+;;; asciidoc-mode.el --- Major mode for AsciiDoc files -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 Dunaevskii Maxim
+
+;; Author: Dunaevskii M. <dunmaksim@yandex.ru>
+;; Maintainer: Dunaevskii M. <dunmaksim@yandex.ru>
+;; Created: October 30, 2024
+;; Version: 0.0.1-alpha
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: AsciiDoc
+;; URL: https://github.com/dunmaksim/asciidoc-mode/
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See the README.md file for details.
+
+;;; Code:
+
+(require 'faces)
+(require 'font-lock)
+(require 'rx)
+(require 'asciidoc-faces)
+
+(defconst asciidoc-package-version "0.0.1-alpha")
+
+(defun asciidoc-version ()
+  "Return a package `asciidoc-mode.el' version."
+  (interactive)
+  (message (format "asciidoc-mode version: %s" asciidoc-package-version)))
+
+
+(defgroup asciidoc nil "Support for AsciiDoc documents."
+  :group 'text
+  :version asciidoc-package-version
+  :link '(url-link "https://docs.asciidoctor.org/"))
+
+
+(defcustom asciidoc-mode-hook nil
+  "Hook run when `asciddoc-mode' is turned on.
+The hook for `text-mode' is run before this one."
+  :group 'asciidoc
+  :type '(hook))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; SYNTAX TABLE
+(defvar asciidoc-mode-syntax-table
+  (let ((st (copy-syntax-table text-mode-syntax-table)))
+    (modify-syntax-entry ?$ "." st)
+    (modify-syntax-entry ?% "." st)
+    (modify-syntax-entry ?& "." st)
+    (modify-syntax-entry ?' "." st)
+    (modify-syntax-entry ?` "\"`  " st)
+    (modify-syntax-entry ?* "\"*  " st)
+    (modify-syntax-entry ?+ "." st)
+    (modify-syntax-entry ?- "." st)
+    (modify-syntax-entry ?/ "." st)
+    (modify-syntax-entry ?< "." st)
+    (modify-syntax-entry ?= "." st)
+    (modify-syntax-entry ?> "." st)
+    (modify-syntax-entry ?\\ "\\" st)
+    (modify-syntax-entry ?_ "." st)
+    (modify-syntax-entry ?| "." st)
+    (modify-syntax-entry ?« "." st)
+    (modify-syntax-entry ?» "." st)
+    (modify-syntax-entry ?‘ "." st)
+    (modify-syntax-entry ?’ "." st)
+    (modify-syntax-entry ?“ "." st)
+    (modify-syntax-entry ?” "." st)
+    st)
+  "Syntax table used while in `asciidoc-mode'.")
+
+(defvar asciidoc--font-lock-keywords
+  '(("^= .+$" . asciidoc-face-header-1)
+    ("^== .+$" . asciidoc-face-header-2)
+    ("^=== .+$" . asciidoc-face-header-3)
+    ("^==== .+$" . asciidoc-face-header-4)
+    ("^===== .+$" . asciidoc-face-header-5)
+    ("^====== .+$" . asciidoc-face-header-6)
+    ("\\*.+\\*" . asciidoc-face-bold)
+    ("_.+_" . asciidoc-face-emphasis)
+    ("`.+`" . asciidoc-face-inline-code)
+
+    ;; footnote:[Text]
+    ;; 1 - footnote
+    ;; 2 - :
+    ;; 3 - [
+    ;; 4 - Text
+    ;; 5 - ]
+    ("\\(footnote\\)\\(:\\)\\(\\[\\)\\(.+\\)\\(\\]\\)"
+     (1 'asciidoc-face-footnote)
+     (2 'asciidoc-face-punctuation)
+     (3 'asciidoc-face-bracket)
+     (4 'asciidoc-face-footnote-text)
+     (5 'asciidoc-face-bracket))
+
+    ;; NOTE: Text
+    ;; 1 - NOTE:
+    ("^\\(NOTE:\\) .+$"
+     (1 'asciidoc-face-note))
+
+    ;; [NOTE]
+    ("^\\[NOTE\\]$" . asciidoc-face-note)
+
+    ;; TIP: Text
+    ;; 1 - TIP
+    ;; 2 - :
+    ;; 3 -  Text
+    ("^\\(TIP\\)\\(:\\)\\( .+\\)$"
+     (1 'asciidoc-face-tip)
+     (2 'asciidoc-face-punctuation)
+     (3 'asciidoc-face-default))
+
+    ;; [TIP]
+    ;; 1 - [
+    ;; 2 - TIP
+    ;; 3 - ]
+    ("^\\(\\[\\)\\(TIP\\)\\(\\]\\)$"
+     (1 'asciidoc-face-bracket)
+     (2 'asciidoc-face-tip)
+     (3 'asciidoc-face-bracket))
+
+    ;; IMPORTANT: Text
+    ;; 1 - IMPORTANT
+    ;; 2 - :
+    ;; 3 -  Text
+    ("^\\(IMPORTANT\\)\\(:\\)\\( .+\\)$"
+     (1 'asciidoc-face-important)
+     (2 'asciidoc-face-punctuation)
+     (3 'asciidoc-face-default))
+
+    ;; [IMPORTANT]
+    ;; 1 - [
+    ;; 2 - IMPORTANT
+    ;; 3 -]
+    ("^\\(\\[\\)\\(IMPORTANT\\)\\(\\]\\)$"
+     (1 'asciidoc-face-bracket)
+     (2 'asciidoc-face-important)
+     (3 'asciidoc-face-bracket))
+
+    ;; CAUTION: Text
+    ;; 1 - CAUTION
+    ;; 2 - :
+    ;; 3 - Text
+    ("^\\(CAUTION\\)\\(:\\)\\( .+\\)$"
+     (1 'asciidoc-face-caution)
+     (2 'asciidoc-face-punctuation)
+     (3 'asciidoc-face-default))
+
+    ;; [CAUTION]
+    ;; 1 - [
+    ;; 2 - CAUTION
+    ;; 3 -]
+    ("^\\(\\[\\)\\(CAUTION\\)\\(\\]\\)$"
+     (1 'asciidoc-face-bracket)
+     (2 'asciidoc-face-caution)
+     (3 'asciidoc-face-bracket))
+
+    ;; WARNING: Text
+    ;; 1 - WARNING
+    ;; 2 - :
+    ;; 3 -  Text
+    ("^\\(WARNING\\)\\(:\\)\\( .+\\)$"
+     (1 'asciidoc-face-warning)
+     (2 'asciidoc-face-punctuation)
+     (3 'asciidoc-face-default))
+
+    ;; [WARNING]
+    ;; 1 - [
+    ;; 2 - WARNING
+    ;; 3 -]
+    ("^\\(\\[\\)\\(WARNING\\)\\(\\]\\)$"
+     (1 'asciidoc-face-bracket)
+     (2 'asciidoc-face-warning)
+     (3 'asciidoc-face-bracket))
+
+    ;; kbd:[C-x C-c]
+    ;; 1 - kbd
+    ;; 2 - :
+    ;; 3 - [
+    ;; 4 - C-x C-c
+    ;; 5 - ]
+    ("\\(kbd\\)\\(:\\)\\(\\[\\)\\(.+\\)\\(\\]\\)"
+     (1 'asciidoc-face-kbd)
+     (2 'asciidoc-face-punctuation)
+     (3 'asciidoc-face-bracket)
+     (4 'asciidoc-face-kbd-text)
+     (5 'asciidoc-face-bracket))
+
+    ;; Block macro
+    ;; name::value[text]
+    ;; 1 - name
+    ;; 2 - ::
+    ;; 3 - value
+    ;; 4 - [
+    ;; 5 - text
+    ;; 6 - ]
+    ("^(.+)(::)(.+)(\\[)(.*)(\\])"
+     (1 'asciidoc-face-macro-name)
+     (2 'asciidoc-face-punctuation)
+     (3 'asciidoc-face-text)
+     (4 'asciidoc-face-bracket)
+     (5 'asciidoc-face-macro-value)
+     (6 'asciidoc-face-bracket))
+
+    ;; Block comment:
+    ;; 1. //// - comment delimiter
+    ;; 2. Commentary - commentary
+    ;; 3. //// - comment delimiter
+    ("^\\(////\n\\)\\(.+\n\\)\\(////\\)$"
+     (1 'asciidoc-face-comment-delimiter)
+     (2 'asciidoc-face-comment)
+     (3 'asciidoc-face-comment-delimiter))
+
+    ;; One line commentary
+    ;; // Commentary
+    ("^// .*$" asciidoc-face-comment)
+
+    ;; ID:
+    ;; [#id]
+    ;; 1 - [
+    ;; 2 - #id
+    ;; 3 - ]
+    ("^\\(\\[\\)\\(#.+\\)\\(\\]\\)"
+     (1 'asciidoc-face-bracket)
+     (2 'asciidoc-face-id)
+     (3 'asciidoc-face-bracket))
+    )
+  "Default `font-lock-keywords' for `asciidoc-mode'.")
+
+;;;###autoload
+(define-derived-mode asciidoc-mode text-mode "AsciiDoc"
+  "Major mode for editing AsciiDoc documents.
+
+Turning on `asciidoc-mode' calls the normal hooks `text-mode-hook'
+and `asciidoc-mode-hook'.  This mode also support font-lock
+highlighting."
+  :syntax-table asciidoc-mode-syntax-table
+  :group 'asciidoc
+  (setq-local comment-start "// ")
+  (setq-local indent-tabs-mode nil)
+
+  ;; Font lock.
+  (setq-local font-lock-defaults '(asciidoc--font-lock-keywords t nil nil nil)))
+
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.adoc\\'" . asciidoc-mode))
+
+
+(provide 'asciidoc-mode)
+
+;;; asciidoc-mode.el ends here
